4140e1c3789d022acc98de845e7bb9815609bd68
amine <amineparis97x@gmail.com>
2023-07-25 13:38:51 +0100

    modification css et photographers factory

--- scripts/factory/photographersFactory.js
+++ scripts/factory/photographersFactory.js
@@ -1,44 +1,59 @@
 const photographersFactory = (data) => {
     const { name, id, city, country, tagline, price, portrait } = data;
     const picture = `assets/photographers/${portrait}`;
-
+  
     function getUserCardDOM() {
-        const article = document.createElement('article');
-        article.classList.add("card");
-
-        const img = document.createElement('img');
-        img.setAttribute("src", picture);
-
-        const photographeNameElement = document.createElement('h2');
-        photographeNameElement.classList.add("card__photographeName");
-        photographeNameElement.textContent = name;
-
-        const cityElement = document.createElement('p');
-        cityElement.classList.add("card__city");
-        cityElement.textContent = city +", "+country;
-
-        const tagLineElement = document.createElement('p');
-        tagLineElement.classList.add("card__tagline");
-        tagLineElement.textContent = tagline;
-
-        const priceElement = document.createElement('p');
-        priceElement.classList.add("card__price");
-        priceElement.textContent = price+" €";
-
-        article.appendChild(img);
-        article.appendChild(photographeNameElement); 
-        article.appendChild(cityElement);
-        article.appendChild(tagLineElement);
-        article.appendChild(priceElement);
-
-        return article;
+      const article = document.createElement("article");
+      article.classList.add("card");
+  
+      const photographerLinkElement = document.createElement("a");
+      photographerLinkElement.classList.add("card__link");
+      photographerLinkElement.setAttribute("href", `pages/photographer.html?id=${id}`);
+      photographerLinkElement.setAttribute("aria-label", `Lien vers le portfolio de ${name}`);
+  
+      const photographerImgElement = document.createElement("img");
+      photographerImgElement.classList.add("card__img");
+      photographerImgElement.setAttribute("src", picture);
+      photographerImgElement.setAttribute("alt", `Photo de ${name}`);
+  
+      const photographerNameElement = document.createElement("h2"); 
+      photographerNameElement.classList.add("card__photographeName");
+      photographerNameElement.textContent = name;
+  
+      const cityElement = document.createElement("p");
+      cityElement.classList.add("card__city");
+      cityElement.textContent = city + ", " + country;
+  
+      const tagLineElement = document.createElement("p");
+      tagLineElement.classList.add("card__tagline");
+      tagLineElement.textContent = tagline;
+  
+      const priceElement = document.createElement("p");
+      priceElement.classList.add("card__price");
+      priceElement.textContent = price + " €";
+  
+      photographerLinkElement.appendChild(photographerImgElement);
+      photographerLinkElement.appendChild(photographerNameElement);
+  
+      article.appendChild(photographerLinkElement);
+      article.appendChild(cityElement);
+      article.appendChild(tagLineElement);
+      article.appendChild(priceElement);
+  
+      return article;
     }
+  
+    return {
+      name,
+      id,
+      city,
+      country,
+      tagline,
+      price,
+      portrait,
+      getUserCardDOM,
+    };
+  };
+  
 
-    return { name, id, city, country, tagline, price, portrait, getUserCardDOM };
-}
-
-const photographersProfilFactory = (data) => {
-
-
-    
-}
+const photographersProfilFactory = (data) => {};
