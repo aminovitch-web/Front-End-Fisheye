fbf31355e7cc0f657cb5884b148fcce8d0dd08f1
amine <amineparis97x@gmail.com>
2023-08-10 11:09:35 +0100

    modification photographer.js

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -22,10 +22,85 @@ const getPhotographerById = async (id) => {
       return null;
     }
   } catch (error) {
-    console.log("Error while retrieving photographer:", error);
+    console.log(error);
     return null;
   }
 };
 
+const getMediaByPhotographer = async (id) => {
+    try{
+      const photographersJson = await getAllDataService();
+      const medias = photographersJson.media;
 
-getPhotographerById(id);
+      const mediasData = medias.find((m) => m.photographerId === parseInt(id));
+
+      if(mediasData){
+        console.log("media photograph:", mediasData);
+        return mediasData;
+      } else{
+        console.log("photographer not found", id);
+        return null;
+      }
+    }catch(error) {
+      console.log( error);
+      return null;
+    }
+}
+
+const displayPhotographHeaderData = async (photographerInformation) => {
+  let photographerCardHeader = document.querySelector(".photograph-header");
+  const photographerArray = Array.isArray(photographerInformation)
+    ? photographerInformation
+    : Object.values(photographerInformation);
+
+  photographerArray.forEach((photographerInfo) => {
+    const photographerModel = photographerFactory(photographerInfo);
+    const photographerCardHeaderElement = photographerModel.getPhotographerCardHeader(photographerInfo);
+    photographerCardHeader.appendChild(photographerCardHeaderElement);
+  });
+}
+
+
+
+
+const displayMedia = async (mediaPhotographer) => {
+  let mediaSection = document.querySelector(".medias");
+  const photographerArray = Array.isArray(mediaPhotographer)
+    ? mediaPhotographer
+    : Object.values(mediaPhotographer);
+
+  photographerArray.forEach((mediaPhotographer) => {
+    const photographerModel = mediaFactory(mediaPhotographer);
+    const photographerMedia = photographerModel.getMediaDom();
+    mediaSection.appendChild(photographerMedia);
+  });
+}
+
+const getTotalLikesCard = async(id) => {
+    try{
+    const photographersJson = await getAllDataService();
+    const medias = photographersJson.media;
+
+    const totalLikes = medias.filter(media => media.photographerId === id).reduce((acc,media) => acc+media.likes,0);
+   return totalLikes;
+
+    }catch(error){
+      console.log(error)
+    }
+}
+
+
+const init = async () => {
+ try{
+
+  const photographerInformation = await getPhotographerById(id);
+  displayPhotographHeaderData(photographerInformation);
+  console.log(photographerInformation);
+
+ } catch (error) {
+   console.error("Erreur lors de l'initialisation : ", error);
+ }
+
+
+}
+
