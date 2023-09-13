5d240e2e430c4fb6b13cc102486f6481139902dc
amine <amineparis97x@gmail.com>
2023-09-13 12:05:46 +0100

    verification bug

--- scripts/services/photographersServices.js
+++ scripts/services/photographersServices.js
@@ -1,17 +1,17 @@
-export async function getAllDataService () {
-  const URL = 'data/photographers.json'
+export async function getAllDataService() {
+    const URL = "data/photographers.json";
 
-  try {
-    const response = await fetch(URL)
+    try {
+        const response = await fetch(URL);
 
-    if (response.ok) {
-      return response.json()
-    } else {
-      console.error('Erreur de réponse :', response.status)
-      return []
+        if (response.ok) {
+            return response.json();
+        } else {
+            console.error("Erreur de réponse :", response.status);
+            return [];
+        }
+    } catch (error) {
+        console.error("Erreur lors de la récupération des données :", error);
+        return false;
     }
-  } catch (error) {
-    console.error('Erreur lors de la récupération des données :', error)
-    return false
-  }
 }
