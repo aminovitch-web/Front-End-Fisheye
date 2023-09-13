343d5a5fd0998619096be0de1439b6995b291f9b
amine <amineparis97x@gmail.com>
2023-09-13 11:18:22 +0100

    correction erreur w3

--- scripts/utils/validateForm.js
+++ scripts/utils/validateForm.js
@@ -1,17 +1,17 @@
-function validateForm() {
-    const prenom = document.getElementById("prenom").value;
-    const nom = document.getElementById("nom").value;
-    const email = document.getElementById("email").value;
-    const message = document.getElementById("message").value;
+export const validateForm = () => {
+    const prenom = document.getElementById('prenom').value
+    const nom = document.getElementById('nom').value
+    const email = document.getElementById('email').value
+    const message = document.getElementById('message').value
 
-    console.log("Nom : " + nom);
-    console.log("Prénom : " + prenom);
-    console.log("Email : " + email);
-    console.log("Message : " + message);
+    console.log('Nom : ' + nom)
+    console.log('Prénom : ' + prenom)
+    console.log('Email : ' + email)
+    console.log('Message : ' + message)
 
     // Masquer la modale
-    const modal = document.getElementById('contact_modal');
-    modal.style.display = 'none';
-    document.body.style.overflow = 'auto';
-    document.getElementById('main').style.filter = 'none';
+    const modal = document.getElementById('contact_modal')
+    modal.style.display = 'none'
+    document.body.style.overflow = 'auto'
+    document.getElementById('main').style.filter = 'none'
 }
